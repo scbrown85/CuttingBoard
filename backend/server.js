be6b75e93d936d6e47bd616186dfae698fd304e5
Stewart Brown <stewartbrown60@proton.me>
2023-08-19 19:16:40 -0700

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,38 @@
- 
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/users');
+const mealPlanRoutes = require('./routes/mealPlans');
+const shoppingListRoutes = require('./routes/shoppingLists');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Welcome message for the root endpoint
 app.get('/', (req, res) => {
   res.send('Welcome to CuttingBoard.ai backend!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-const mongoose = require('mongoose');
+// User-related routes
+app.use('/users', userRoutes);
 
+// Meal plan-related routes
+app.use('/mealPlans', mealPlanRoutes);
+
+// Shopping list-related routes
+app.use('/shoppingLists', shoppingListRoutes);
+
+// Connect to MongoDB
 mongoose.connect('mongodb://localhost/cuttingboard', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
