3262e93c3037368e3490da8f49e6a69deeb38132
Stewart Brown <stewartbrown60@proton.me>
2023-08-19 23:00:17 -0700

    Updated some macronutrient intake test logic

--- backend/controllers/macronutrientIntakeController.js
+++ backend/controllers/macronutrientIntakeController.js
@@ -1,24 +1,36 @@
  
 // Calculate BMI
 function calculateBMI(weight, height) {
+    if (weight <= 0 || height <= 0) return null;
     return weight && height ? (weight / (height * height)) * 703 : null;
   }
   
-  // Calculate Body Fat Percentage
-  function calculateBodyFatPercentage(gender, height, waist, hips, neck) {
+  function calculateBodyFatPercentage(gender, waist, hips, neck, height) {
+    // Check for missing or invalid values
+    if (!gender || !waist || !neck || !height || (gender === 'Female' && !hips)) {
+      return null;
+    }
+  
+    // Check for zero or negative values in the logarithm arguments
+    if (height <= 0 || (gender === 'Male' && waist - neck <= 0) || (gender === 'Female' && waist + hips - neck <= 0)) {
+      return null;
+    }
+  
     if (gender === 'Male') {
-      return 86.01 * Math.log10(waist - neck) - 70.041 * Math.log10(height) + 36.76;
+      return (86.01 * Math.log10(waist - neck) - 70.041 * Math.log10(height) + 36.76) / 100;
     } else if (gender === 'Female') {
-      return 163.205 * Math.log10(waist + hips - neck) - 97.684 * Math.log10(height) - 78.387;
+      return (163.205 * Math.log10(waist + hips - neck) - 97.684 * Math.log10(height) - 78.387) / 100;
     } else {
       return null; // Handle error or missing values
     }
   }
   
+  
   // Calculate BMR
   function calculateBMR(age, height, weight) {
-    return weight && height && age ? (weight * (1 / 2.2)) * 10 + 6.25 * (height * 2.54) - 5 * age + 5 : null;
+    return weight > 0 && height > 0 && age > 0 ? (weight * (1 / 2.2)) * 10 + 6.25 * (height * 2.54) - 5 * age + 5 : null;
   }
+  
   
   // Calculate Starting TDEE
   function calculateStartingTDEE(activityLevel, BMR) {
@@ -29,29 +41,69 @@ function calculateBMI(weight, height) {
       'Very Active': 1.675,
       'Extra Active': 1.875,
     };
-    return activityLevel && BMR ? BMR * activityMultiplier[activityLevel] : null;
+  
+    if (activityLevel && BMR && activityMultiplier[activityLevel] !== undefined) {
+      return BMR * activityMultiplier[activityLevel];
+    } else {
+      return null;
+    }
   }
+  
   
   // Calculate Daily Caloric Intake
   function calculateDailyCaloricIntake(goalWeight, currentWeight, goalDate, startDate, TDEE) {
+    if (!goalWeight || !currentWeight || !goalDate || !startDate || !TDEE || goalDate < startDate) {
+      return null;
+    }
     const daysToGoal = (goalDate - startDate) / (1000 * 60 * 60 * 24);
     return (500 * goalWeight - 500 * currentWeight) / (daysToGoal / 7) + TDEE;
   }
   
+  
   // Calculate Protein Intake
   function calculateProteinIntake(gender, goalWeight, currentWeight, bodyFatPercentage) {
-    // Logic based on provided formula
+    if (gender === 'Male') {
+      if (goalWeight < currentWeight) {
+        if (bodyFatPercentage >= 0.25) return currentWeight * 0.6;
+        if (bodyFatPercentage <= 0.08) return currentWeight * 1.2;
+        return currentWeight * ((-3 * bodyFatPercentage) + 1.35);
+      } else {
+        if (bodyFatPercentage > 0.25) return currentWeight * 0.8;
+        if (bodyFatPercentage > 0.16 && bodyFatPercentage <= 0.25) return currentWeight * 0.9;
+        return currentWeight;
+      }
+    } else { // Female
+      if (goalWeight < currentWeight) {
+        if (bodyFatPercentage >= 0.32) return currentWeight * 0.6;
+        if (bodyFatPercentage <= 0.18) return currentWeight * 1.2;
+        return currentWeight * ((-3.75 * bodyFatPercentage) + 1.8);
+      } else {
+        if (bodyFatPercentage > 0.32) return currentWeight * 0.8;
+        if (bodyFatPercentage > 0.23 && bodyFatPercentage <= 0.32) return currentWeight * 0.9;
+        return currentWeight;
+      }
+    }
   }
+  
+  
   
   // Calculate Fats Intake
   function calculateFatsIntake(goalWeight, currentWeight) {
-    // Logic based on provided formula
+    if (goalWeight === null || currentWeight === null) return null; // Handling unexpected cases
+    if (goalWeight < currentWeight) return currentWeight * 0.3;
+    if (goalWeight > currentWeight) return currentWeight * 0.4;
+    if (goalWeight === currentWeight) return currentWeight * 0.35;
+    return null;
   }
+  
+  
   
   // Calculate Carbohydrates Intake
   function calculateCarbohydratesIntake(caloricIntake, protein, fats) {
+    if (caloricIntake === null || protein === null || fats === null) return null;
     return (caloricIntake - (protein * 4 + fats * 9)) / 4;
   }
+  
   
   // Calculate Estimated Weight Change Per Week
   function calculateWeightChangePerWeek(caloricIntake, TDEE) {
--- backend/tests/macronutrientIntakeController.test.js
+++ backend/tests/macronutrientIntakeController.test.js
@@ -87,31 +87,188 @@ const {
               });
   
     describe('calculateBMR', () => {
-      // Tests for calculateBMR
-    });
+        describe('calculateBMR', () => {
+            it('should calculate BMR correctly', () => {
+              const age = 25; // Example age in years
+              const height = 68; // Example height in inches
+              const weight = 150; // Example weight in pounds
+              const expectedBMR = (weight * (1 / 2.2)) * 10 + 6.25 * (height * 2.54) - 5 * age + 5; // Expected BMR value
+          
+              const result = calculateBMR(age, height, weight);
+          
+              expect(result).toBeCloseTo(expectedBMR, 1); // Allowing 1 decimal place tolerance
+            });
+          
+            it('should return null if age, height, or weight is missing', () => {
+              expect(calculateBMR(null, 68, 150)).toBeNull();
+              expect(calculateBMR(25, null, 150)).toBeNull();
+              expect(calculateBMR(25, 68, null)).toBeNull();
+            });
+          
+            it('should return null if age, height, or weight is zero or negative', () => {
+              expect(calculateBMR(0, 68, 150)).toBeNull();
+              expect(calculateBMR(25, 0, 150)).toBeNull();
+              expect(calculateBMR(25, 68, 0)).toBeNull();
+              expect(calculateBMR(-25, 68, 150)).toBeNull();
+              expect(calculateBMR(25, -68, 150)).toBeNull();
+              expect(calculateBMR(25, 68, -150)).toBeNull();
+            });
+          });
+              });
   
     describe('calculateStartingTDEE', () => {
-      // Tests for calculateStartingTDEE
-    });
+        describe('calculateStartingTDEE', () => {
+            it('should calculate starting TDEE correctly', () => {
+              const BMR = 1500; // Example BMR value
+              const activityLevel = 'Moderate'; // Example activity level
+              const expectedTDEE = BMR * 1.475; // Expected TDEE value based on the "Moderate" activity level
+          
+              const result = calculateStartingTDEE(activityLevel, BMR);
+          
+              expect(result).toBeCloseTo(expectedTDEE, 1); // Allowing 1 decimal place tolerance
+            });
+          
+            it('should return null if BMR or activity level is missing', () => {
+              expect(calculateStartingTDEE('Moderate', null)).toBeNull();
+              expect(calculateStartingTDEE(null, 1500)).toBeNull();
+            });
+          
+            it('should return null if activity level is invalid', () => {
+              expect(calculateStartingTDEE('InvalidLevel', 1500)).toBeNull();
+            });
+          });
+              });
   
-    describe('calculateDailyCaloricIntake', () => {
-      // Tests for calculateDailyCaloricIntake
-    });
+              describe('calculateDailyCaloricIntake', () => {
+                describe('calculateDailyCaloricIntake', () => {
+                  it('should calculate daily caloric intake correctly for weight loss', () => {
+                    const goalWeight = 140;
+                    const currentWeight = 150;
+                    const goalDate = new Date('2023-12-31');
+                    const startDate = new Date('2023-01-01');
+                    const TDEE = 2000;
+                    const expectedCaloricIntake = 1903.85; // Updated expected value
+              
+                    const result = calculateDailyCaloricIntake(goalWeight, currentWeight, goalDate, startDate, TDEE);
+              
+                    expect(result).toBeCloseTo(expectedCaloricIntake, 2); // Allowing 2 decimal places tolerance
+                  });
+              
+                  it('should calculate daily caloric intake correctly for weight gain', () => {
+                    const goalWeight = 160;
+                    const currentWeight = 150;
+                    const goalDate = new Date('2023-12-31');
+                    const startDate = new Date('2023-01-01');
+                    const TDEE = 2000;
+                    const expectedCaloricIntake = 2096.15; // Updated expected value
+              
+                    const result = calculateDailyCaloricIntake(goalWeight, currentWeight, goalDate, startDate, TDEE);
+              
+                    expect(result).toBeCloseTo(expectedCaloricIntake, 2); // Allowing 2 decimal places tolerance
+                  });
+              
+                  it('should return null if any required parameter is missing', () => {
+                    expect(calculateDailyCaloricIntake(null, 150, new Date('2023-12-31'), new Date('2023-01-01'), 2000)).toBeNull();
+                    expect(calculateDailyCaloricIntake(140, null, new Date('2023-12-31'), new Date('2023-01-01'), 2000)).toBeNull();
+                    expect(calculateDailyCaloricIntake(140, 150, null, new Date('2023-01-01'), 2000)).toBeNull();
+                    expect(calculateDailyCaloricIntake(140, 150, new Date('2023-12-31'), null, 2000)).toBeNull();
+                    expect(calculateDailyCaloricIntake(140, 150, new Date('2023-12-31'), new Date('2023-01-01'), null)).toBeNull();
+                  });
+              
+                  it('should return null if goal date is before start date', () => {
+                    const goalDate = new Date('2023-01-01');
+                    const startDate = new Date('2023-12-31');
+              
+                    expect(calculateDailyCaloricIntake(140, 150, goalDate, startDate, 2000)).toBeNull();
+                  });
+                });
+              });
+              
   
-    describe('calculateProteinIntake', () => {
-      // Tests for calculateProteinIntake
-    });
+              describe('calculateProteinIntake', () => {
+                it('should calculate protein intake for male with weight loss goal', () => {
+                  expect(calculateProteinIntake('Male', 150, 200, 0.3)).toBe(120);
+                });
+              
+                it('should calculate protein intake for male with weight gain goal', () => {
+                  expect(calculateProteinIntake('Male', 200, 150, 0.15)).toBe(150); // Current weight
+                });
+              
+                it('should calculate protein intake for female with weight loss goal', () => {
+                  expect(calculateProteinIntake('Female', 120, 150, 0.35)).toBe(90);
+                });
+              
+                it('should calculate protein intake for female with weight gain goal', () => {
+                  expect(calculateProteinIntake('Female', 150, 120, 0.22)).toBe(120); // Current weight
+                });
+              });
+              
   
-    describe('calculateFatsIntake', () => {
-      // Tests for calculateFatsIntake
-    });
+              describe('calculateFatsIntake', () => {
+                it('should calculate fats intake for weight loss goal', () => {
+                  expect(calculateFatsIntake(150, 200)).toBe(60); // 200 * 0.3
+                });
+              
+                it('should calculate fats intake for weight gain goal', () => {
+                  expect(calculateFatsIntake(200, 150)).toBe(60); // 150 * 0.4
+                });
+              
+                it('should calculate fats intake for maintaining weight', () => {
+                  expect(calculateFatsIntake(150, 150)).toBe(52.5); // 150 * 0.35
+                });
+              
+                it('should handle unexpected cases', () => {
+                  expect(calculateFatsIntake(null, 150)).toBeNull(); // Handling unexpected cases
+                });
+              });
+              
   
-    describe('calculateCarbohydratesIntake', () => {
-      // Tests for calculateCarbohydratesIntake
-    });
+              describe('calculateCarbohydratesIntake', () => {
+                it('should calculate carbohydrates intake correctly', () => {
+                  expect(calculateCarbohydratesIntake(2000, 100, 50)).toBe(287.5);
+                });
+              
+                it('should return 0 if caloricIntake is equal to the sum of protein and fats calories', () => {
+                  expect(calculateCarbohydratesIntake(1000, 100, 50)).toBe(37.5);
+                });
+              
+                it('should handle negative values for caloricIntake, protein, and fats', () => {
+                    expect(calculateCarbohydratesIntake(-2000, -100, -50)).toBe(-287.5);
+                  });
+                  
+              
+                it('should return null if any parameter is missing', () => {
+                  expect(calculateCarbohydratesIntake(null, 100, 50)).toBeNull();
+                  expect(calculateCarbohydratesIntake(2000, null, 50)).toBeNull();
+                  expect(calculateCarbohydratesIntake(2000, 100, null)).toBeNull();
+                });
+              });
+              
   
     describe('calculateEstimatedWeightChange', () => {
-      // Tests for calculateEstimatedWeightChange
-    });
+        describe('calculateWeightChangePerWeek', () => {
+            it('should calculate weight loss per week', () => {
+              expect(calculateWeightChangePerWeek(2000, 2500)).toBe(-1); // Weight loss of 1 unit per week
+            });
+          
+            it('should calculate weight gain per week', () => {
+              expect(calculateWeightChangePerWeek(2500, 2000)).toBe(1); // Weight gain of 1 unit per week
+            });
+          
+            it('should return 0 if caloricIntake is equal to TDEE', () => {
+              expect(calculateWeightChangePerWeek(2000, 2000)).toBe(0); // No weight change
+            });
+          
+            it('should handle negative values for caloricIntake and TDEE', () => {
+              expect(calculateWeightChangePerWeek(-2000, -2000)).toBe(0); // No weight change with negative values
+              expect(calculateWeightChangePerWeek(-2500, -2000)).toBe(-1); // Weight loss of 1 unit per week with negative values
+            });
+          
+            it('should return null if any parameter is missing', () => {
+              expect(calculateWeightChangePerWeek(null, 2000)).toBeNull();
+              expect(calculateWeightChangePerWeek(2000, null)).toBeNull();
+            });
+          });
+              });
   });
   
